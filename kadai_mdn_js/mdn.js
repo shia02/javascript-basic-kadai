fd2b9c2a4da03bfca277d5c8fc6b80b3814c692e
shia <nahsor2242195@gmail.com>
2025-02-09 19:10:55 +0900

    fix month

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,5 +1,5 @@
 let today = new Date();
 let year = today.getFullYear();
 let day = today.getDate();
-let month = today.getMonth();
+let month = today.getMonth() + 1;
 console.log(year + '年' + month + '月' + day + '日');
