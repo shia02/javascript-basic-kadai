f64daa073dbc79ef5ce48a8470d9b9b8fa67d777
shia <nahsor2242195@gmail.com>
2025-02-10 14:01:06 +0900

    kadai_014

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 const personalData = { name: '侍太郎', age: 36, gender: '男性' };
 personalData.address = '40'
-console.log(personalData.address);
+
+const calc = (price) => {
+    return price + 5;
+}
+
+let num = 5;
+calc(num);
+console.log(calc(num));
