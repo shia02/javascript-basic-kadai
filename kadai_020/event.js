d62646943076538083c7f350fd414f916da669bf
shia <nahsor2242195@gmail.com>
2025-02-11 14:38:23 +0900

    fix

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
 const clickBtn = document.getElementById('btn');
 clickBtn?.addEventListener('click', () => {
     const text = document.getElementById('text');
-    const newText = document.createElement('h2');
-    newText.textContent = 'ボタンをクリックしました';
-    text?.replaceWith(newText);
+    if (text) {
+        text.innerHTML = 'ボタンをクリックしました';
+    }
 });
