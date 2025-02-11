d4cef5e826d5affaede42f4d04348e495780b399
shia <nahsor2242195@gmail.com>
2025-02-11 14:29:41 +0900

    kadai_020

--- js/script.js
+++ js/script.js
@@ -1,10 +1,55 @@
-const personalData = { name: '侍太郎', age: 36, gender: '男性' };
-personalData.address = '40'
+const btn = document.getElementById('output-btn');
 
-const calc = (price) => {
-    return price + 5;
+btn?.addEventListener('click', () => {
+    console.log('click!');
+});
+
+const btn2 = document.getElementById('add-btn');
+const parentList = document.getElementById('parent-list');
+btn2?.addEventListener('click', () => {
+    const element = document.createElement('li');
+    element.textContent = 'new list';
+    parentList?.appendChild(element);
+});
+
+const count = document.getElementById('count-btn');
+count?.addEventListener('click', () => {
+    // @ts-ignore
+    const value = document.forms.textForm.textBox.value;
+    console.log(value.length + '文字');
+});
+
+const areaBtn = document.getElementById('area-btn');
+areaBtn?.addEventListener('click', () => {
+    // @ts-ignore
+    const value = document.forms.areaForm.area.value;
+    console.log(value);
+});
+
+const checkBtn = document.getElementById('os-btn');
+checkBtn?.addEventListener('click', () => {
+    // @ts-ignore
+    const checkbox = document.forms.osForm.os;
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked) {
+            console.log(checkbox[i].value);
+        }
+    }
+});
+
+const backBtn = document.getElementById('back-btn');
+// window.addEventListener('scroll', () => {
+//     const scrollValue = document.scrollingElement?.scrollTop ?? 0;
+//     if (scrollValue >= 300) {
+//         backBtn.style.display = 'inline';
+//     } else {
+//         backBtn.style.display = 'none';
+//     }
+// });
+
+if (backBtn) {
+    window.addEventListener('scroll', () => {
+        const scrollValue = document.scrollingElement?.scrollTop ?? 0;
+        backBtn.style.display = scrollValue >= 300 ? 'inline' : 'none';
+    });
 }
-
-let num = 5;
-calc(num);
-console.log(calc(num));
